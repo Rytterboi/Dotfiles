f61a3e46dabef9b38aaac802be3f6a177e1a6985
rytter <rytter@rerail.dk>
2024-09-04 20:55:03 +0200

    dadbod cmp attemted and wezterm opacity reduction

--- .config/nvim/lua/josean/plugins/nvim-cmp.lua
+++ .config/nvim/lua/josean/plugins/nvim-cmp.lua
@@ -45,18 +45,23 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp"},
+        { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
-
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
         }),
+      },
+    })
+    cmp.setup.filetype({ "sql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
       },
     })
   end,
