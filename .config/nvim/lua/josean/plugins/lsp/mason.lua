bab449c2933b4e11dcc9ef3d05d8c191a6d9d871
rytter <rytter@rerail.dk>
2024-09-05 12:55:21 +0200

    tsserver - vtsls cuz of deprecation warning

--- .config/nvim/lua/josean/plugins/lsp/mason.lua
+++ .config/nvim/lua/josean/plugins/lsp/mason.lua
@@ -27,7 +27,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
@@ -38,6 +37,7 @@ return {
         "prismals",
         "pyright",
         "omnisharp",
+        "vtsls",
       },
     })
 
