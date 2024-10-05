fb70e1c2ddfe86cc69753176ecffefaf8139a1c1
rytter <rytter@rerail.dk>
2024-10-05 12:09:00 +0200

    neogit test

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1072,6 +1072,18 @@ require('lazy').setup({
     },
   },
 
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
+
   -- the following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. if you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
