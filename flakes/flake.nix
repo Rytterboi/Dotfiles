4daa0410148dbe265be659dae037caa509d9b94f
rytter <rytter.jakob@gmail.com>
2025-05-03 12:06:35 +0200

    Add flake.nix and host configs

---
+++ flakes/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; # Change this to update packages
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+  in
+  {
+    nixosConfigurations = {
+      DIY-Desktop = nixpkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          ./hosts/DIY-Desktop/configuration.nix
+          ./hosts/DIY-Desktop/hardware-configuration.nix
+        ];
+      };
+      T480 = nixpkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          ./hosts/T480/configuration.nix
+          ./hosts/T480/hardware-configuration.nix
+        ];
+      };
+    };
+  };
+}
+
