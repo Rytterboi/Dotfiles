1f2eeff6f1f6c2d2418a172f569d994e1c2db95b
rytter <rytter.jakob@gmail.com>
2025-12-13 22:50:42 +0100

    Mullvad

--- flakes/hosts/T480/configuration.nix
+++ flakes/hosts/T480/configuration.nix
@@ -48,6 +48,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Add mullvad vpn
+  services.mullvad-vpn = { enable = true; package = pkgs.mullvad-vpn; };
+  with services.resolved.enable = true;
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
